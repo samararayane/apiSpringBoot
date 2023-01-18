e11de4f6968b929f6bf435cb34352a120507f69b
Samara Rayane (Samy) <samararayane@hotmail.com>
2023-01-18 15:09:10 -0300

    Project started

---
+++ src/test/java/br/com/treinamento/api_springBoot/ApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinamento.api_springBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
