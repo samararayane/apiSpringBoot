666d1329659eefa6b9a894ef169fcfad9b46d6a6
Samara Rayane (Samy) <samararayane@hotmail.com>
2023-01-18 15:50:03 -0300

    class user

---
+++ src/main/java/br/com/treinamento/api_springBoot/domain/User.java
@@ -0,0 +1,22 @@
+package br.com.treinamento.api_springBoot.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
