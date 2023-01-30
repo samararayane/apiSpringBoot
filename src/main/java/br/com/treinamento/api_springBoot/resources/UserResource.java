65c4ed41716409ef11c6eaac5346dd51d2405012
Samara Rayane (Samy) <samararayane@hotmail.com>
2023-01-30 10:40:55 -0300

    Exceçao para email já cadastrado na atualizaçao

--- src/main/java/br/com/treinamento/api_springBoot/resources/UserResource.java
+++ src/main/java/br/com/treinamento/api_springBoot/resources/UserResource.java
@@ -7,25 +7,53 @@ import br.com.treinamento.api_springBoot.services.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.Servlet;
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
 
-    protected ModelMapper modelMapper;
+    private static final String ID = "/{id}";
 
     @Autowired
-    private ModelMapperConfig mapper;
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private ModelMapper mapper;
 
     @Autowired
     private UserService service;
 
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = ID)
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(modelMapper.map(service.findById(id), UserDTO.class));
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll() {
+        return ResponseEntity.ok().body(service.findAll()
+                .stream()
+                .map(x -> mapper.map(x, UserDTO.class))
+                .collect(Collectors.toList()));
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> create(@RequestBody UserDTO obj) {
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path(ID)
+                .buildAndExpand(service.create(obj).getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping(value = ID)
+    public ResponseEntity<UserDTO> update(@PathVariable Integer id, @RequestBody UserDTO obj) {
+        obj.setId(id);
+        return ResponseEntity.ok().body(mapper.map(service.update(obj), UserDTO.class));
     }
 }
